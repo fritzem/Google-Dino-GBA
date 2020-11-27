5ad6c1d118c0a2a43e8947b63aa8704852f88f55
fritzem <50849645+fritzem@users.noreply.github.com>
2020-11-27 14:30:34 -0500

    Hello world

---
+++ build/template.d
@@ -0,0 +1,15 @@
+template.o: C\:/dev/GBA/dino/source/template.c \
+ C\:/devkitPro/libgba/include/gba_console.h \
+ C\:/devkitPro/libgba/include/gba_base.h \
+ C\:/devkitPro/libgba/include/gba_types.h \
+ C\:/devkitPro/libgba/include/gba_video.h \
+ C\:/devkitPro/libgba/include/gba_interrupt.h \
+ C\:/devkitPro/libgba/include/gba_systemcalls.h \
+ C\:/devkitPro/libgba/include/gba_input.h
+C\:/devkitPro/libgba/include/gba_console.h:
+C\:/devkitPro/libgba/include/gba_base.h:
+C\:/devkitPro/libgba/include/gba_types.h:
+C\:/devkitPro/libgba/include/gba_video.h:
+C\:/devkitPro/libgba/include/gba_interrupt.h:
+C\:/devkitPro/libgba/include/gba_systemcalls.h:
+C\:/devkitPro/libgba/include/gba_input.h:
