896a09b2dd93e11137836ce98c06bd6e9e0e79d1
fritzem <50849645+fritzem@users.noreply.github.com>
2021-01-02 23:06:50 -0500

    removed garbage

--- source/dino.c
+++ source/dino.c
@@ -307,24 +307,3 @@ void addPoint(int add, int *base, int *point) {
 bool randomBool() {
 	return ((qran_range(0, 1000) > 500) ? true : false);
 }
-
-//bitmap display
-void bitDisp()
-{
-    REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
-
-    m3_mem[80][120]= CLR_RED;
-    m3_mem[80][136]= CLR_LIME;
-    m3_mem[96][120]= CLR_BLUE;
-
-    while(1) {
-    	vid_vsync();
-    	m3_mem[80][120]= CLR_BLUE;
-
-    	key_poll();
-    	if (key_is_down(KEY_A))
-    	{
-    		m3_mem[80][120]= CLR_RED;
-    	}
-    }
-}
